4854764794397c0473926f8d5504545fbc0e8e2c
Alexander Lamb <lambalex@leto21.iro.umontreal.ca>
2017-06-23 19:46:58 -0400

    updates to use more realistic length

--- scan_test.py
+++ scan_test.py
@@ -2,16 +2,15 @@ import theano
 import theano.tensor as T
 import numpy as np
 
-k = T.iscalar("k")
 A = T.ivector("A")
 
 #ns = 10
 
-shared_tensor = T.zeros(shape=(10,))
+shared_tensor = T.zeros(shape=(1000,),dtype=theano.config.floatX)
 
 def onestep(a,shared_boy):
 
-    shared_boy = T.set_subtensor(shared_boy[a],a)
+    shared_boy = T.set_subtensor(shared_boy[a],T.cast(a,'float32'))
     return a + 1, shared_boy
     
 
@@ -19,7 +18,7 @@ def onestep(a,shared_boy):
 result, updates = theano.scan(fn=onestep,
                               outputs_info=[T.ones_like(A),shared_tensor],
                               non_sequences=[],
-                              n_steps=k)
+                              n_steps=800)
 
 # We only care about A**k, but scan has provided us with A**1 through A**k.
 # Discard the values that we don't care about. Scan is smart enough to
@@ -30,12 +29,12 @@ shared = result[1]
 # compiled function that returns A**k
 import time
 t0 = time.time()
-power = theano.function(inputs=[A,k], outputs=[final_result,shared], updates=updates)
+power = theano.function(inputs=[A], outputs=[final_result,shared], updates=updates)
 print "time 2 compile", time.time() - t0
 
 print "running"
-print(power(range(10),2))[1].shape
-print(power(range(10),4))[1].shape
+print(power(range(1000)))[1].shape
+print(power(range(1000)))[1].shape
 
 
 
